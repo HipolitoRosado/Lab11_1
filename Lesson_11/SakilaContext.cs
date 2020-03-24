7b460cec11a11a9f0e8d708053c872c7ceab505f
HipolitoRosado <paulserrano58@gmail.com>
2020-03-24 13:51:06 -0500

    Add project files.

---
+++ Lesson_11/SakilaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lesson_11
+{
+    class SakilaContext : DbContext
+    {
+        public DbSet<Actor> Actor { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost\sqlexpress;Database=sakila;Trusted_Connection=True;");
+        }
+    }
+}
+
+
